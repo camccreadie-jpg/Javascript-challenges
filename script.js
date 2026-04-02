3b243046ff210470f6ef44b844aacbf34e45ad20
camccreadie-jpg. <camccreadie@gmail.com.>
2026-04-01 20:46:04 -1000

    Test commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// function falsyOrTruthy(elem1, elem2 ) {
+//     return elem1 ? elem1 : elem2;
+// }
+
+// console.log(falsyOrTruthy(0, 0));
+
+
+// function falsyOrTruthy(elem1, elem2 ) {
+//     return (elem1) ? elem1 : elem2;
+// }
+
+// console.log(falsyOrTruthy(0, 0));
+
+
+// function arrLength(arr) {
+//     return arr. ength;
+// }
+
+// console.log(arrLength([1, 2, 3, 4]));
+
+
+// function arrLength(arr) {
+//     return arr[arr.length - 1] ;
+// }
+
+// console.log(arrLength([1, 2, 3, 4]));
+
+
+function arrSum(arr) {
+    for (let i = 0; i < arr.length; ++i) {
+        console.log('run this code')
+    }
+    console.log('for loop broke')
+}
+
+console.log(arrSum([1, 2, 3, 4]));
